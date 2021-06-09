6f423d61a1e6d58d898932abb7547f7e252e7439
Derek M Berger <dmberger.dev@gmail.com>
2021-06-08 20:32:21 -0400

    summarize shapes

---
+++ data/niis/log_shapes.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from pandas import DataFrame
+from pathlib import Path
+from ants import image_read
+from tqdm.contrib.concurrent import process_map
+from typing import Tuple
+
+NIIS = Path(__file__).resolve().parent.rglob("*.nii.gz")
+
+def get_shape(nii: Path):
+    return (*image_read(str(nii)).shape, nii)
+
+if __name__ == "__main__":
+    rets = process_map(get_shape, NIIS)
+    dfs = []
+    for ret in rets:
+        h, w, d, t, nii = rets
+        dfs.append(DataFrame(
+            dict(
+                H=h,
+                W=w,
+                D=d,
+                T=t
+            )
+        ), index=[str(nii.name)])
+    df = pd.concat(dfs, axis=0)
+    print(df)
+    outfile = Path(__file__).resolve().parent / "shapes.json"
+    df.to_json(outfile)
+    print(f"Saved shape data to {outfile}")
+
+
+
