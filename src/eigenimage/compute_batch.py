1872a9c5ea42dc6801b78d26b7dc38c2e76a54fc
Derek M Berger <dmberger.dev@gmail.com>
2021-06-16 14:26:27 -0400

    update batch size to match new compute times

--- src/eigenimage/compute_batch.py
+++ src/eigenimage/compute_batch.py
@@ -9,21 +9,24 @@ import sys
 from argparse import ArgumentParser
 from pathlib import Path
 from typing import List
-import pandas as pd
 
+import pandas as pd
 
 sys.path.append(str(Path(__file__).resolve().parent.parent.parent))
 from src.constants import NII_PATH
 from src.eigenimage.compute import compute_eigenimage
-
 
 SHAPES = NII_PATH / "shapes.json"
 
 # we'll be cutting the timepoints to the last 176, which means computations
 # take very close to 2 hours each time, so 11 computations per 24h window,
 # i.e. 11 computations per batch
+MAX_JOB_TIME = 24  # hours
+EIGIMG_COMPUTE_TIME = 60 / 3  # new masked images take about 15 minutes for 176 timepoints
 T_LENGTH = 176
-BATCH_SIZE = 10
+
+# computed / derived based on constants above
+BATCH_SIZE = int(MAX_JOB_TIME / EIGIMG_COMPUTE_TIME) - 1  # -1 to be careful / give extra time
 
 
 def get_batch_idx() -> int:
