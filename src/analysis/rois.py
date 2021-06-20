fb1fba9e3e6662f1efdeec64f03a838f5da26537
Derek M Berger <dmberger.dev@gmail.com>
2021-06-20 16:38:59 -0400

    generalize reduction

--- src/analysis/rois.py
+++ src/analysis/rois.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from argparse import Namespace
+from dataclasses import dataclass
 from pathlib import Path
 from typing import Any, Callable, Dict, List, Optional, Tuple, Type, Union, cast, no_type_check
 
@@ -23,6 +24,7 @@ sys.path.append(str(Path(__file__).resolve().parent.parent.parent))
 from src.eigenimage.compute_batch import T_LENGTH
 
 DATA = Path(__file__).resolve().parent.parent.parent / "data"
+NIIS = DATA / "niis"
 ROIS = DATA / "rois"
 if not ROIS.exists():
     os.makedirs(ROIS, exist_ok=True)
@@ -34,6 +36,56 @@ EIGIMGS = DATA / "eigimgs"
 ATLAS_DIR = DATA / "atlases"
 ATLAS = ATLAS_DIR / "cc400_roi_atlas_ALIGNED.nii.gz"
 LEGEND = ATLAS_DIR / "CC400_ROI_labels.csv"
+
+
+@dataclass
+class RoiReduction:
+    """Reduce each ROI to a single sequence (possibly of length 1) via `reducer`
+
+    Parameters
+    ----------
+    source: Literal["func", "eigimg"]
+        Whether we are reducing the original functional or eigenimages
+    nii: Path
+        Path to eigimg.nii.gz file
+    label: int
+        int class label
+    legend: DataFrame
+        legend from `parse_legend`
+    eigens: path
+        path to full eigenvalues for normalization
+    norm: literal["div", "diff"]
+        if "div", divide by the full eigenvalues. this places all vales in [0, 1].
+        if "diff", subtract the full eigenvalues.
+    reducer: callable[[ndarray], ndarray] = none
+        reducing function. if none, uses np.mean(axis=0). must operate on an array
+        of shape (roi_voxels, t) and produce a vector of shape (l,).
+    reducer_name: str = none
+        label which determines output directory of reducer. if `none`, will try
+        to use `reducer.__name__`.
+    """
+
+    source: Literal["func", "eigimg"]
+    nii: Path
+    label: int
+    legend: DataFrame
+    eigens: Path
+    norm: Optional[Literal["div", "diff"]] = "div"
+    reducer: Optional[Callable[[ndarray], ndarray]] = None
+    reducer_name: Optional[str] = None
+
+
+def mean(x: ndarray) -> ndarray:
+    return np.mean(x, axis=0)
+
+def median(x: ndarray) -> ndarray:
+    return np.median(x, axis=0)
+
+def std(x: ndarray) -> ndarray:
+    return np.std(x, ddof=1, axis=0)
+
+def max(x: ndarray) -> ndarray:
+    return np.max(x, axis=0)
 
 
 def parse_legend(legend: Path) -> DataFrame:
@@ -64,50 +116,110 @@ def subject_labels(imgs: List[Path]) -> List[int]:
     return labels
 
 
-def compute_roi_means(args: Namespace) -> DataFrame:
-    """return DataFrame where rows are ROIs, columns are:
+def compute_subject_roi_reductions(
+    args: RoiReduction,
+) -> DataFrame:
+    """Reduce each ROI to a single sequence (possibly of length 1) via `reducer`
 
-    "name":     (ROI name),
-    "n_voxels": (how many in roi),
-    "signal":   (mean signal across voxels)
-    "class":    (0=CTRL, 1=AUTISM)
+    Parameters
+    ----------
+    args: RoiReduction
+        args.nii: Path
+            Path to eigimg.nii.gz file
+        args.label: int
+            int class label
+        args.legend: DataFrame
+            legend from `parse_legend`
+        args.eigens: Path
+            Path to full eigenvalues for normalization
+        args.norm: Literal["div", "diff"]
+            If "div", divide by the full eigenvalues. This places all vales in [0, 1].
+            If "diff", subtract the full eigenvalues.
+        args.reducer: Callable[[ndarray], ndarray] = None
+            Reducing function. If None, uses np.mean(axis=0). Must operate on an array
+            of shape (ROI_voxels, T) and produce a vector of shape (L,).
+        args.reducer_name: str = None
+            Label which determines output directory of reducer. If `None`, will try
+            to use `reducer.__name__`.
+
+    Returns
+    -------
+    reduced: DataFrame
+        Where rows are ROIs, columns are:
+
+            "name":     (ROI name),
+            "n_voxels": (how many in roi),
+            "signal":   (mean signal across voxels)
+            "class":    (0=CTRL, 1=AUTISM)
     """
-    np.warnings.filterwarnings("ignore", category=np.VisibleDeprecationWarning)
+    np.warnings.filterwarnings("ignore", category=np.VisibleDeprecationWarning)  # type: ignore
     nii, label, legend, eigens = args.nii, args.label, args.legend, args.eigens
-    eigs = np.load(eigens)
-    if eigs.shape != (T_LENGTH - 1,):
+    reducer, reducer_name = args.reducer, args.reducer_name
+    source, norm = args.source, args.norm
+    if reducer is None:
+        reducer = mean
+
+    raw = nib.load(str(nii)).get_fdata()
+    if (raw.shape[-1] - 1) != (T_LENGTH - 1):
         return
-    # img = nib.load(str(nii)).get_fdata() - eigs  # normalize
-    img = eigs / nib.load(str(nii)).get_fdata()
+    if norm == "div" and source == "eigimg":
+        eigs = np.load(eigens)
+        img = eigs / raw
+    elif norm == "diff" and source == "eigimg":
+        eigs = np.load(eigens)
+        img = raw - eigs
+    else:
+        norm = None
+        img = raw
     atlas = nib.load(str(ATLAS)).get_data()
+
     df = DataFrame(index=legend.index, columns=["name", "n_voxels", "signal"])
     for id in zip(legend.index):
         mask = atlas == id
         roi = img[mask, :]
-        # roi = img[mask, 126]
         n_voxels = roi.shape[0]
-        mean = np.mean(roi, axis=0)
-        # mean = np.std(roi, axis=0, ddof=1)
+        reduction = reducer(roi)
         name = legend.loc[id].item()
-        df.loc[id, :] = (name, n_voxels, mean)
+        df.loc[id, :] = (name, n_voxels, reduction)
     extensions = "".join(nii.suffixes)
-    outdir = ROIS / ("ctrl" if label == 0 else "autism")
+    subdir = "ctrl" if label == 0 else "autism"
+    rname = reducer.__name__ if reducer_name is None else reducer_name
+    outdir = ROIS / f"{source}/{rname}/{subdir}"
+    if not outdir.exists():
+        os.makedirs(outdir, exist_ok=True)
     outfile = outdir / nii.name.replace(extensions, ".parquet").replace(
-        "_func_minimal", "_ROI_means"
+        "_func_minimal", f"_ROI_{rname}_norm={norm}"
     )
     df.to_parquet(str(outfile), index=True)
 
 
-def compute_roi_mean_signals() -> None:
-    niis = sorted(EIGIMGS.rglob("*eigimg.nii.gz"))
+def compute_all_subject_roi_reductions(
+    source: Literal["func", "eigimg"] = "eigimg",
+    norm: Literal["div", "diff"] = "div",
+    reducer: Callable[[ndarray], ndarray] = None,
+    reducer_name: str = None,
+) -> None:
+    if source == "eigimg":
+        niis = sorted(EIGIMGS.rglob("*eigimg.nii.gz"))
+    else:
+        niis = sorted(NIIS.rglob("*func_minimal.nii.gz"))
     eigs = [EIGS / nii.name.replace("_eigimg.nii.gz", ".npy") for nii in niis]
     labels = subject_labels(niis)
     legend = parse_legend(LEGEND)
     args = [
-        Namespace(**dict(nii=nii, label=label, legend=legend, eigens=eig))
+        RoiReduction(
+            source=source,
+            nii=nii,
+            label=label,
+            legend=legend,
+            eigens=eig,
+            norm=norm,
+            reducer=reducer,
+            reducer_name=reducer_name,
+        )
         for nii, eig, label in zip(niis, eigs, labels)
     ]
-    process_map(compute_roi_means, args)
+    process_map(compute_subject_roi_reductions, args)
 
 
 def eig_descriptives() -> DataFrame:
@@ -145,28 +257,39 @@ def eig_descriptives() -> DataFrame:
 def cohens_d(x1: DataFrame, x2: DataFrame) -> float:
     n1, n2 = len(x1) - 1, len(x2) - 1
     sd_pool = (n1 * np.std(x1, ddof=1) + n2 * np.std(x2, ddof=1)) / (n1 + n2)
-    return (np.mean(x1) - np.mean(x2)) / sd_pool
+    return float((np.mean(x1) - np.mean(x1)) / sd_pool)
 
 
 def auc(x1: DataFrame, x2: DataFrame) -> float:
     y_true = np.concatenate([np.zeros_like(x1), np.ones_like(x2)])
     y_score = np.concatenate([x1, x2])
-    return roc_auc_score(y_true, y_score)
+    return float(roc_auc_score(y_true, y_score))
 
 
-def compute_roi_largest_descriptive_stats(n_largest: int = 1) -> DataFrame:
-    autisms = [pd.read_parquet(p) for p in sorted((ROIS / "autism").rglob("*.parquet"))]
-    ctrls = [pd.read_parquet(p) for p in sorted((ROIS / "ctrl").rglob("*.parquet"))]
+def compute_roi_largest_descriptive_stats(
+    source: Literal["func", "eigimg"] = "eigimg",
+    norm: Literal["div", "diff"] = "div",
+    reducer: Callable[[ndarray], ndarray] = None,
+    reducer_name: str = None,
+    slicer: slice = slice(-1),
+    slice_reducer: Callable[[ndarray], ndarray] = mean,
+) -> DataFrame:
+    if reducer is None:
+        reducer = mean
+    rname = reducer.__name__ if reducer_name is None else reducer_name
+    regex = f"*_ROI_{rname}_norm={norm}.parquet"
+    autism_dir = ROIS / f"{source}/{rname}/autism"
+    ctrl_dir = ROIS / f"{source}/{rname}/ctrl"
+    autisms = [pd.read_parquet(p) for p in sorted(autism_dir.rglob(regex))]
+    ctrls = [pd.read_parquet(p) for p in sorted(ctrl_dir.rglob(regex))]
     names = autisms[0]["name"].copy()
     n_voxels = autisms[0]["n_voxels"].copy()
 
     for df in autisms:
-        # df.signal = df.signal.apply(lambda s: s[:-n_largest].mean())
-        df.signal = df.signal.apply(lambda s: s[n_largest].max())
+        df.signal = df.signal.apply(lambda s: slice_reducer(s[slicer]))
         df.drop(columns=["name", "n_voxels"], inplace=True)
     for df in ctrls:
-        # df.signal = df.signal.apply(lambda s: s[:-n_largest].mean())
-        df.signal = df.signal.apply(lambda s: s[n_largest].max())
+        df.signal = df.signal.apply(lambda s: slice_reducer(s[slicer]))
         df.drop(columns=["name", "n_voxels"], inplace=True)
 
     autism = pd.concat(autisms, axis=1)
@@ -182,7 +305,7 @@ def compute_roi_largest_descriptive_stats(n_largest: int = 1) -> DataFrame:
         ac = auc(ctr, aut)
         descriptives.iloc[roi, :] = (t, t_p, U, U_p, d, ac)
     descriptives.index = names
-    print(f"Largest differences")
+    print("Largest differences")
     print(
         descriptives.sort_values(by=["U_p", "d"], ascending=True)
         .iloc[:20, :]
@@ -205,7 +328,13 @@ if __name__ == "__main__":
     #     .sort_values(by="U_p", ascending=True)
     #     .to_markdown(tablefmt="simple", floatfmt=["1.2f", "1.2f", "1.1e", "1.2f", "1.1e","1.3f", "1.3f"])
     # )
-    # compute_roi_mean_signals()
+    for reducer in [mean, median, max, std]:
+        compute_all_subject_roi_reductions(
+            source="func",
+            norm="div",
+            reducer=reducer,
+        )
+    sys.exit()
     dfs = []
     for i in range(1, 30):
         df = compute_roi_largest_descriptive_stats(n_largest=-i)
