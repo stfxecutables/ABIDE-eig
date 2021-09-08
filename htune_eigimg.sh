2685828566eaa1016b92f8c6544ffcf4f76e4e6d
Derek Berger <dberger@login2.mun.ace-net.ca>
2021-09-08 18:58:25 +0000

    change eigimg size

--- htune_eigimg.sh
+++ htune_eigimg.sh
@@ -5,7 +5,7 @@ python src/analysis/predict/deep_learning/generate_jobscripts.py \
     --max_epochs=125 \
     --batch_size=25 \
     --val_batch_size=20 \
-    --slice_start=100 \
+    --slice_start=125 \
     --slice_end=175 \
     --is_eigimg
 sbatch job_scripts/submit_htune_eigimg.sh
