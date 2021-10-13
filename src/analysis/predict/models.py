7122d203462e55310e8c9a1b31a1db8c2d9c44b9
Derek M Berger <dmberger.dev@gmail.com>
2021-10-13 19:14:31 -0400

    parallelize

--- src/analysis/predict/models.py
+++ src/analysis/predict/models.py
@@ -6,11 +6,14 @@ sys.path.append(str(ROOT))
 # fmt: on
 
 from math import floor
+from typing import Dict, Optional
 
 import optuna
 import pandas as pd
 from pandas import DataFrame
+from sklearn.model_selection import ParameterGrid
 from tqdm import tqdm
+from tqdm.contrib.concurrent import process_map
 
 from src.analysis.features import FEATURES, NormMethod
 from src.analysis.predict.hypertune import (
@@ -38,40 +41,51 @@ Classic ML: SVM, LR, RF AdaBoostDTree
 Deep Learn: MLP, Conv1D, ...?
 """
 
+
+def fit_to_args(args: Dict) -> Optional[Dict]:
+    f = args["feature"]
+    norm = args["norm"]
+    slice_min = args["slice_min"]
+    slice_max = args["slice_max"]
+    if slice_max <= slice_min:
+        return None
+    # print(f"Fitting {f} using normalization method {norm}")
+    x, y = f.load(norm)
+    smin = floor(slice_min * x.shape[1])
+    smax = floor(slice_max * x.shape[1]) if slice_max != 1.0 else None
+    sl = slice(smin, smax)
+    x = x[:, sl]
+    # htune_result = hypertune_classifier(
+    #     "lda", x, y, n_trials=1, verbosity=optuna.logging.ERROR
+    # )
+    # result = evaluate_hypertuned(htune_result, 5, x, y, log=False)
+    result = evaluate_untuned("rf", 5, x, y, log=False)
+    return {
+        "feature": f.name,
+        "norm": norm.value,
+        "smin": slice_min,
+        "smax": slice_max,
+        "acc": result["acc"],
+    }
+
+
 if __name__ == "__main__":
     features = [f for f in FEATURES if len(f.shape_data.shape) == 1]
-    fnames, accs, norms, smins, smaxs = [], [], [], [], []
-    for f in tqdm(features, leave=True):
-        for norm in tqdm([NormMethod.S_MINMAX, NormMethod.F_MINMAX, NormMethod.F_STD], leave=True):
-            for slice_min in tqdm([0, 0.25, 0.50, 0.75, 0.9], leave=False):
-                for slice_max in [0.50, 0.75, 0.9, 1.0]:
-                    if slice_max <= slice_min:
-                        continue
-                    # print(f"Fitting {f} using normalization method {norm}")
-                    x, y = f.load(norm)
-                    smin = floor(slice_min * x.shape[1])
-                    smax = floor(slice_max * x.shape[1]) if slice_max != 1.0 else None
-                    sl = slice(smin, smax)
-                    x = x[:, sl]
-                    # htune_result = hypertune_classifier(
-                    #     "lda", x, y, n_trials=1, verbosity=optuna.logging.ERROR
-                    # )
-                    # result = evaluate_hypertuned(htune_result, 5, x, y, log=False)
-                    result = evaluate_untuned("rf", 5, x, y, log=False)
-                    accs.append(result["acc"])
-                    norms.append(norm.value)
-                    smins.append(slice_min)
-                    smaxs.append(slice_max)
-                    fnames.append(f.name)
-    df = DataFrame(
-        data={
-            "feature": fnames,
-            "norm": norms,
-            "slice_start": smins,
-            "slice_end": smaxs,
-            "acc": accs,
-        }
+    grid = list(
+        ParameterGrid(
+            dict(
+                feature=features,
+                norm=[NormMethod.S_MINMAX, NormMethod.F_MINMAX, NormMethod.F_STD],
+                slice_min= [0, 0.25, 0.50, 0.75, 0.9],
+                slice_max=[0.50, 0.75, 0.9, 1.0],
+                # slice_min=[0],
+                # slice_max=[0.50],
+            )
+        )
     )
+    results = process_map(fit_to_args, grid, max_workers=8)
+    dfs = [DataFrame(info, index=[0]) for info in results]
+    df = pd.concat(dfs, axis=0, ignore_index=True)
     df.to_parquet(ROOT / "htune_initial_results_LDA.parquet")
     pd.options.display.max_rows = 999
     print(df.sort_values(by="acc", ascending=False))
